db2074fbe296aefdbb168dba3b0b52e6227c7d4f
OksanaBebesh <oksana_bebesh1984@bk.ru>
2020-09-30 13:48:27 +0300

    feat: make task Array Problems add min max avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  
+
+  return (Array.isArray(array) && array.length) ? [...array].sort((a,b)=>a-b)[0]:0;;
 }
 
 exports.max = function max (array) {
-  return 0;
+ 
+  return (Array.isArray(array) && array.length) ? [...array].sort((a,b)=>b-a)[0]:0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let result = 0;
+ 
+  if (Array.isArray(array) && array.length){
+      result =  array.reduce((total, amount, index, array)=>{
+          total += amount;
+            if (index === array.length - 1){
+              return total/array.length;
+            }else {
+              return total;
+            }
+      });
+  }
+   return result;
 }
